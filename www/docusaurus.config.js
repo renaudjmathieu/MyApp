b141ec9ea3a9b697c3f358bd8557fb6719fca25e
Renaud Mathieu <renaudjmathieu@gmail.com>
2022-11-13 13:44:11 -0500

    colormode

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -62,6 +62,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
       navbar: {
         title: 'Renaud Mathieu',
         logo: {
