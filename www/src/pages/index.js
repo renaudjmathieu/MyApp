b9402b736b577bc69adb56f45b8653e7a1fd71cf
Renaud Mathieu <renaudjmathieu@gmail.com>
2022-11-12 22:12:03 -0500

    a

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -15,6 +15,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
         <Image img={logo} className={styles.heroImg} />
       </div>
     </header>
