705f426f78bb0b1b898845df8008f7789f60aab2
Renaud Mathieu <renaudjmathieu@gmail.com>
2022-11-16 01:14:55 -0500

    .

--- www/src/index.js
+++ www/src/index.js
@@ -10,12 +10,15 @@ import {
   NavBar,
   Footer,
 } from "./components";
+import Bob from './pages/Bob';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
     <NavBar />
+    <Bob />
     <Routes>
       <Route path="/" element={<Home />} />
       
---
+++ www/src/pages/bob.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+function Bob() {
+
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await (await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return (
+    <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+          {data}
+        </p>
+      </div>
+  );
+}
+export default Bob;
