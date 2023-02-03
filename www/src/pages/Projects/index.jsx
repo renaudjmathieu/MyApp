5dc61cde0ea074ab0ba0ea8c931130e2daeba876
Renaud Mathieu <renaudjmathieu@gmail.com>
2023-02-02 21:27:59 -0500

    add d3-elephant mini-project

--- www/src/pages/Projects/index.jsx
+++ www/src/pages/Projects/index.jsx
@@ -20,17 +20,31 @@ function Projects() {
         <Container>
           <section className={styles.features}>
             <Row>
-              <Col lg={12} ><Card style={{ width: '24rem' }}>
+              <Col lg={4} >
+                <Card>
                 <Card.Body>
                   <Card.Title>Data Visualization with D3 and React</Card.Title>
                   <Card.Img variant="top" className="project-image" src="https://raw.githubusercontent.com/renaudjmathieu/my-web-app/main/www/src/img/project1.png" />
                   <Card.Text>
-                    A dynamic and configurable data dashboard web app built with D3 + React.
+                    A data dashboard app built using D3 + React.
                   </Card.Text>
                   <Card.Link className="dumbLink" href="https://red-dune-0c424950f.2.azurestaticapps.net/" target="_blank">Demo</Card.Link>
                   <Card.Link className="dumbLink" href="https://github.com/renaudjmathieu/data-visualization-d3-react" target="_blank">GitHub</Card.Link>
                 </Card.Body>
-              </Card></Col>
+              </Card>
+              </Col>
+              <Col lg={4} ><Card>
+                <Card.Body>
+                  <Card.Title>Data Visualization with D3 + React</Card.Title>
+                  <Card.Img variant="top" className="project-image" src="https://raw.githubusercontent.com/renaudjmathieu/my-web-app/main/www/src/img/d3-elephant.png" />
+                  <Card.Text>
+                    A simple app drawn with D3 + React.
+                  </Card.Text>
+                  <Card.Link className="dumbLink" href="https://proud-cliff-07e30880f.2.azurestaticapps.net/" target="_blank">Demo</Card.Link>
+                  <Card.Link className="dumbLink" href="https://github.com/renaudjmathieu/d3-elephant" target="_blank">GitHub</Card.Link>
+                </Card.Body>
+              </Card>
+              </Col>
             </Row>
           </section>
         </Container>
