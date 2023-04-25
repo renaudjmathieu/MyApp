1800a01070ed5439a0a4ca38753d08d2093a9866
Renaud Mathieu <renaudjmathieu@gmail.com>
2023-04-25 00:23:17 -0400

    simple and clean

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react"
 import Header from "./Header/Header"
 import Projects from "./../Projects/Projects"
-import Footer from "./Footer/Footer"
 
 const Home = () => (
   <>
       <div className="Home">
         <Header />
-        <Footer />
       </div>
   </>
 )
